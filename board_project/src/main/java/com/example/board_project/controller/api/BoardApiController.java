f38ee73e916fa2a6fcc4e041a326bbfe9a3355b2
shin is god <shindudwns@naver.com>
2023-06-28 15:14:21 +0900

    보드 게시판 메인화면 출력
    보드 게시판 메인화면 출력 및 db저장까지 완료

---
+++ board_project/src/main/java/com/example/board_project/controller/api/BoardApiController.java
@@ -0,0 +1,24 @@
+package com.example.board_project.controller.api;
+
+import com.example.board_project.config.auth.PrincipalDetail;
+import com.example.board_project.dto.BoardSaveDto;
+import com.example.board_project.entity.User;
+import com.example.board_project.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.parameters.P;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class BoardApiController {
+
+    @Autowired
+    private BoardService boardService;
+    @PostMapping("/board/save")
+    public String boardSave(BoardSaveDto boardSaveDto, @AuthenticationPrincipal PrincipalDetail principalDetail) {
+        User user = principalDetail.getUser();
+        boardService.join(boardSaveDto,user);
+        return "redirect:/";
+    }
+}
