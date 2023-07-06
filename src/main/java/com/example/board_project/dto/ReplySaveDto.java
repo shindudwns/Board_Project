3912bfc5d409737ccb031c197d49fc2edf65aeed
shin is god <shindudwns@naver.com>
2023-07-07 00:44:13 +0900

    게시물 댓글 작성 기능 추가 23.7.07(3)

---
+++ src/main/java/com/example/board_project/dto/ReplySaveDto.java
@@ -0,0 +1,16 @@
+package com.example.board_project.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+public class ReplySaveDto {
+
+    private String content;
+}
